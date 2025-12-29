1f0be7ed5da45fbd171109a4b77398897fea76a1
MILAN DHANGAR <143923189+milandhangar123@users.noreply.github.com>
2025-12-29 22:34:26 +0530

    Update SignUp.jsx

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Signup.css';
+import './SignUp.css';
 
 const Signup = ({ setIsLoggedIn }) => {
   const [formData, setFormData] = useState({
