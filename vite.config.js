1a4c93dc8467f45137cdb77fb866f86f9d1951a8
MILAN DHANGAR <143923189+milandhangar123@users.noreply.github.com>
2025-12-31 17:13:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,32 +6,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-
-    // ✅ Use esbuild only
     minify: 'esbuild',
 
-    // ✅ esbuild way to remove console/debugger
     esbuild: {
       drop: ['console', 'debugger'],
     },
-
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          axios: ['axios'],
-        },
-      },
-    },
-
-    chunkSizeWarningLimit: 1000,
-  },
-
-  server: {
-    port: 5173,
-  },
-
-  preview: {
-    port: 4173,
   },
 })
